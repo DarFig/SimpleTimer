5f5960d1578ec665075d18ac7a0cc71716c7d90d
DarFig <log.Dariel@gmail.com>
2017-03-26 01:48:01 +0100

    v2017.03.26
    funcionalidad y estilo base

---
+++ README.md
@@ -0,0 +1,8 @@
+# Simple Timer using Electron
+
+###what is this?
+This is a simple timer, created using Electron.
+
+### Requirements
+ - npm
+ - npm install electron
---
+++ func.js
@@ -0,0 +1,28 @@
+var cronotick;
+
+function parar(){
+    clearInterval(cronotick);
+}
+
+
+function arranque(){
+    segundos = 0;
+    minutos = 0;
+    s = document.getElementById("seg");
+    m = document.getElementById("min");
+
+    cronotick = setInterval(
+        function(){
+            if(segundos==60){
+                segundos=0;
+                minutos++;
+                m.innerHTML = minutos;
+                if(minutos==0){
+                    minutos=0;
+                }
+            }
+            s.innerHTML = segundos;
+            segundos++;
+        }
+        , 1000);
+}
